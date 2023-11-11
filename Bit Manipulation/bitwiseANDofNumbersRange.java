7264bee41bb879f3ab7c3a881902333b75d720a4
Rajat Agrawal <45696541+RashakDude@users.noreply.github.com>
2023-11-11 14:52:41 +0000

    Bit manipulation

---
+++ Bit Manipulation/bitwiseANDofNumbersRange.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        if (left == 0 || left == right) {
+            return left;
+        }
+        return left & (~0 << (32 - Integer.numberOfLeadingZeros(left ^ right)));
+    }
+}
